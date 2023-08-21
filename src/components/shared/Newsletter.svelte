990377be33e72f51974b59a8a4cc8bcc98da5b15
Jothish JJ <jothishjj11@gmail.com>
2023-08-21 15:16:32 +0530

    add newsletter component

---
+++ src/components/shared/Newsletter.svelte
@@ -0,0 +1,90 @@
+<div id="mc_embed_shell">
+  <link
+    href="//cdn-images.mailchimp.com/embedcode/classic-061523.css"
+    rel="stylesheet"
+    type="text/css"
+  />
+  <style type="text/css">
+      #mc_embed_signup{backgorund: #fff;false;clear:left; font:14px Helvetica,Arial,sans-serif; width: px;}
+      /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+         We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+  </style>
+  <div
+    id="mc_embed_signup"
+    class="p-4 bg-neutral-100 rounded-lg border-2 shadow-md"
+  >
+    <form
+      action="https://gmail.us14.list-manage.com/subscribe/post?u=c5102ef9765e145b379e913b8&amp;id=6c457d9e89&amp;f_id=00059fe0f0"
+      method="post"
+      id="mc-embedded-subscribe-form"
+      name="mc-embedded-subscribe-form"
+      class="validate"
+      target="_self"
+      novalidate=""
+    >
+      <div id="mc_embed_signup_scroll">
+        <h2>Subscribe to our blog</h2>
+        <div class="mc-field-group">
+          <label for="mce-EMAIL"
+            >Email Address <span class="asterisk">*</span></label
+          ><input
+            type="email"
+            name="EMAIL"
+            class="required email"
+            id="mce-EMAIL"
+            required=""
+            value=""
+          />
+        </div>
+        <div id="mce-responses" class="clear foot">
+          <div
+            class="response"
+            id="mce-error-response"
+            style="display: none;"
+          />
+          <div
+            class="response"
+            id="mce-success-response"
+            style="display: none;"
+          />
+        </div>
+        <div aria-hidden="true" style="position: absolute; left: -5000px;">
+          /* real people should not fill this in and expect good things - do not
+          remove this or risk form bot signups */
+          <input
+            type="text"
+            name="b_c5102ef9765e145b379e913b8_6c457d9e89"
+            tabindex="-1"
+            value=""
+          />
+        </div>
+        <div class="optionalParent">
+          <div class="clear foot">
+            <input
+              type="submit"
+              name="subscribe"
+              id="mc-embedded-subscribe"
+              class="button transition hover:shadow-lg hover:-translate-y-1"
+              value="Subscribe"
+            />
+            <p style="margin: 0px auto;">
+              <a
+                href="http://eepurl.com/ivEv3s"
+                title="Mailchimp - email marketing made easy and fun"
+                ><span
+                  style="display: inline-block; background-color: transparent; border-radius: 4px;"
+                  ><img
+                    class="refferal_badge"
+                    src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg"
+                    alt="Intuit Mailchimp"
+                    style="width: 220px; height: 40px; display: flex; padding: 2px 0px; justify-content: center; align-items: center;"
+                  /></span
+                ></a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
